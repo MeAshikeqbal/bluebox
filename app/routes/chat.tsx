dba9a730e8f0eb7fd45d089e4f5871f2b9f782e4
Ashik Eqbal <hello@itsashik.info>
2024-08-22 02:36:48 +0530

    chore: Update chat layout and resizable panels

--- app/routes/chat.tsx
+++ app/routes/chat.tsx
@@ -1,20 +1,28 @@
 import { Outlet, useLocation } from "@remix-run/react";
 import ChatSideBar from "~/components/chatSideBar";
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "~/components/ui/resizable";
-import EmptyChat from "~/components/chatWindow/emptychat"; // Import your EmptyChat component
+import EmptyChat from "~/components/chatWindow/emptychat";
+
+
 
 export default function Layout() {
-    const location = useLocation();
-
+    const location = useLocation();    
+    
     return (
-        <ResizablePanelGroup direction="horizontal">
-            <ResizablePanel defaultSize={25}>
-                <ChatSideBar />
-            </ResizablePanel>
-            <ResizableHandle className="h-screen" />
-            <ResizablePanel>
-                {location.pathname === "/chat/" ? <EmptyChat /> : <Outlet />}
-            </ResizablePanel>
-        </ResizablePanelGroup>
+        <>
+            <ResizablePanelGroup direction="horizontal">
+                <ResizablePanel defaultSize={25} maxSize={50} minSize={15}>
+                    <ChatSideBar />
+                </ResizablePanel>
+                <ResizableHandle className="h-screen" />
+                <ResizablePanel
+                    className="bg-[url('/background.svg')] bg-cover bg-center h-screen w-screen gackgroud"
+                    defaultSize={75}
+                >
+                    {location.pathname.match(/^\/chat\/?$/) ? <EmptyChat /> : <Outlet />}
+                </ResizablePanel>
+            </ResizablePanelGroup>
+        </>
     );
+
 }
