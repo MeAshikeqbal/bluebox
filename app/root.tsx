d954486cee89374a33caac6cc43784e5581ce91a
Ashik Eqbal <hello@itsashik.info>
2024-10-17 23:26:56 +0530

    feat: Add sign-in and sign-up routes
    Add new sign-in and sign-up routes to the application. This includes the creation of the "SignInPage" and "SignUpPage" components, which render the corresponding forms using the "@clerk/remix" library. The necessary dependencies have also been added to the package.json file.
    
    Changes:
    - Added "SignInPage" component to handle the sign-in route
    - Added "SignUpPage" component to handle the sign-up route
    - Updated package.json to include "@clerk/remix" as a dependency

--- app/root.tsx
+++ app/root.tsx
@@ -6,19 +6,28 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
+import type { MetaFunction, LoaderFunction } from "@remix-run/node";
+
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+import { ClerkApp } from "@clerk/remix";
+
+
+export const meta: MetaFunction = () => ([{
+  charset: "utf-8",
+  title: "BlueBox",
+  viewport: "width=device-width,initial-scale=1",
+}]);
+
+export const loader: LoaderFunction = (args) => rootAuthLoader(args);
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body
-        className="font-sans antialiased "
-      >
+      <body className="font-sans antialiased dark">
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -27,6 +36,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
+function App() {
   return <Outlet />;
 }
+
+export default ClerkApp(App);
