1736bd36359612d81b561cedf9fddb563f4bd0fb
Ashik Eqbal <hello@itsashik.info>
2024-10-17 22:02:59 +0530

    chore: Update tailwind.css, components.json, package-lock.json, package.json, and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,22 @@
 import type { Config } from "tailwindcss"
+import svgToDataUri from "mini-svg-data-uri";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
+interface AddVariablesForColorsParams {
+  addBase: (styles: Record<string, unknown>) => void;
+  theme: (key: string) => unknown;
+}
+
+function addVariablesForColors({ addBase, theme }: AddVariablesForColorsParams) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 
 const config = {
   darkMode: ["class"],
@@ -74,7 +92,21 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: unknown) {
+      matchUtilities(
+        {
+          "bg-dot-thick": (value: unknown) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 } satisfies Config
 
 export default config
